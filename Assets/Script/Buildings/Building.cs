7c657906239eaf52f842d6332d99f0e608d94f8b
Opt <opttsorasit@gmail.com>
2024-02-10 21:11:55 +0700

    Add second unit

--- Assets/Script/Buildings/Building.cs
+++ Assets/Script/Buildings/Building.cs
@@ -26,8 +26,8 @@ public class Building : Structure
     
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.G))
-            ToCreateUnit(0);
+        if (Input.GetKeyDown(KeyCode.G))ToCreateUnit(0);
+        if (Input.GetKeyDown(KeyCode.H))ToCreateUnit(1);
 
         if ((recruitList.Count > 0) && (recruitList[0] != null))
         {
